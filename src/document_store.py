0018d01025f70e98cdc38626907554b047f3b322
gharvey <gato.harvey@defenseunicorns.com>
2024-01-31 12:38:40 -0800

    Cleans up typing, adds refine and raw queries

--- src/document_store.py
+++ src/document_store.py
@@ -1,52 +1,67 @@
 import os
-from typing import List
+import sys
+from typing import List, Mapping
 
 import chromadb
 import httpx
+from chromadb import ClientAPI, GetResult
+from chromadb.api.models import Collection
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.vectorstores import Chroma
+from langchain_core.embeddings import Embeddings
 from llama_index import Response
 from llama_index import VectorStoreIndex, ServiceContext
-from llama_index.llms import OpenAILike
+from llama_index.core.base_query_engine import BaseQueryEngine
+from llama_index.llms import OpenAILike, LLM
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores import ChromaVectorStore
 
-import ingest
+from ingest import Ingest
 
 
 class DocumentStore:
     def __init__(self):
-        self.index_name = "default"
-        self.client = chromadb.PersistentClient(path="db")
-        self.collection = self.client.get_or_create_collection(name="default")
-        self.ingestor = ingest.Ingest(self.index_name, self.client, self.collection)
-        self.chunk_size = 1024
-        self.overlap_size = 20
+        self.client: ClientAPI = chromadb.PersistentClient(path="db")
+        self.default_collection_name: str = "default"
+        self.collection: Collection = self.client.get_or_create_collection(name=self.default_collection_name)
+        self.chunk_size: int = int(os.environ.get('CHUNK_SIZE'))
+        self.overlap_size: int = int(os.environ.get('OVERLAP_SIZE'))
+        self.response_mode: str = os.environ.get('RESPONSE_MODE')
+        self.context_window: int = int(os.environ.get('CONTEXT_WINDOW'))
+        self.max_output: int = int(os.environ.get('MAX_OUTPUT'))
+        self.temperature: float = float(os.environ.get('TEMPERATURE'))
+        self.model: str = os.environ.get('MODEL')
+        self.ingestor: Ingest = Ingest(self.collection, self.chunk_size, self.overlap_size)
 
-        self.embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        self.chroma_db = Chroma(embedding_function=self.embedding_function, collection_name="default",
-                                client=self.client)
+        self.embedding_function: Embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        self.chroma_db: Chroma = Chroma(embedding_function=self.embedding_function,
+                                        collection_name=self.default_collection_name,
+                                        client=self.client)
 
         # LlamaIndex
         api_key: str = os.environ.get('OPENAI_API_KEY')
         api_base: str = os.environ.get('OPENAI_API_BASE')
-        verify_https = False
-        http_client = httpx.Client(verify=verify_https)
-        self.llm = OpenAILike(is_chat_model=True, model="llamacpp", temperature=0.2, max_tokens=8192,
-                              api_base=api_base, api_key=api_key,
-                              http_client=http_client)
-        vector_store = ChromaVectorStore(chroma_collection=self.collection)
-        storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        service_context = ServiceContext.from_defaults(embed_model=self.embedding_function, llm=self.llm,
-                                                       context_window=8192, num_output=1024, chunk_size=self.chunk_size,
-                                                       chunk_overlap=self.overlap_size)
-        self.index = VectorStoreIndex.from_documents(
+        verify_https: bool = os.environ.get('SSL_VERIFICATION').lower() == "true"
+        http_client: httpx.Client = httpx.Client(verify=verify_https)
+        self.llm: LLM = OpenAILike(is_chat_model=True, model=self.model, temperature=self.temperature,
+                                   max_tokens=self.context_window,
+                                   api_base=api_base, api_key=api_key,
+                                   http_client=http_client)
+        vector_store: ChromaVectorStore = ChromaVectorStore(chroma_collection=self.collection)
+        storage_context: StorageContext = StorageContext.from_defaults(vector_store=vector_store)
+        service_context: ServiceContext = ServiceContext.from_defaults(embed_model=self.embedding_function,
+                                                                       llm=self.llm,
+                                                                       context_window=self.context_window,
+                                                                       num_output=self.max_output,
+                                                                       chunk_size=self.chunk_size,
+                                                                       chunk_overlap=self.overlap_size)
+        self.index: VectorStoreIndex = VectorStoreIndex.from_documents(
             [], storage_context=storage_context, service_context=service_context
         )
 
-    def get_all_documents(self):
-        all_documents = self.collection.get(include=['metadatas'])
-        all_metadatas = all_documents['metadatas']
+    def get_all_documents(self) -> dict[str, str]:
+        all_documents: GetResult = self.collection.get(include=['metadatas'])
+        all_metadatas: list[Mapping] = all_documents['metadatas']
         unique_documents: dict[str, str] = {}
         for metadata in all_metadatas:
             unique_documents[metadata['uuid']] = metadata['source']
@@ -57,7 +72,7 @@ class DocumentStore:
             self.collection.delete(where={'uuid': uuid})
 
     # Try catch fails if collection cannot be found
-    def does_collection_exist(self, collection_name):
+    def does_collection_exist(self, collection_name: str) -> bool:
         try:
             collection = self.client.get_collection(name=collection_name)
             if collection.count() > 0:
@@ -67,8 +82,19 @@ class DocumentStore:
 
         return False
 
-    def query_llamaindex(self, query_text):
-        query_engine = self.index.as_query_engine(response_mode="refine")
+    def query_llamaindex(self, query_text: str, response_mode: str = None) -> str | None:
+        if response_mode is None:
+            response_mode = self.response_mode
+
+        query_engine: BaseQueryEngine = self.index.as_query_engine(response_mode=response_mode)
         query_result: Response = query_engine.query(query_text)
 
-        return query_result.response
+        if response_mode == "no_text":
+            query_response: str = query_result.get_formatted_sources(length=sys.maxsize)
+        else:
+            query_response: str = query_result.response
+
+        return query_response
+
+    def load_file_bytes(self, file_bytes: bytes, file_name: str) -> None:
+        self.ingestor.load_file_bytes(file_bytes, file_name)
--- src/ingest.py
+++ src/ingest.py
@@ -41,39 +41,40 @@ def load_file(file_path) -> List[Document]:
         return UnstructuredFileLoader(file_path).load()
 
 
-def update_metadata(file_name, doc_uuid, metadata):
+def update_metadata(file_name: str, doc_uuid: str, metadata: dict) -> dict:
     metadata['source'] = file_name
     metadata['uuid'] = doc_uuid
     return metadata
 
 
-def get_uuids_for_document_texts(texts):
+def get_uuids_for_document_texts(texts: list[Document]) -> list[str]:
     return [str(uuid.uuid4()) for idx in enumerate(texts)]
 
 
 class Ingest:
-    def __init__(self, index_name, client, collection):
-        self.index_name = index_name
-        self.client = client
+    def __init__(self, collection, chunk_size: int, chunk_overlap: int):
         self.collection = collection
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
 
-    def process_file(self, file_name, file_path, chunk_size=1000, chunk_overlap=400):
-        # disallowed_special is set so that api or technical documents that contain special tokens can be loaded
-        text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, disallowed_special=())
+    def process_file(self, file_name: str, file_path: str) -> None:
+        # disallowed_special is set so that technical documents that contain special tokens can be loaded
+        text_splitter = TokenTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap,
+                                          disallowed_special=())
         try:
-            data = load_file(file_path=file_path)
-            texts = text_splitter.split_documents(data)
-            contents = [d.page_content for d in texts]
-            doc_uuid = str(uuid.uuid4())
-            all_metadata = [update_metadata(file_name, doc_uuid, d.metadata) for d in texts]
-            ids = get_uuids_for_document_texts(texts)
+            data: list[Document] = load_file(file_path=file_path)
+            texts: list[Document] = text_splitter.split_documents(data)
+            contents: list[str] = [d.page_content for d in texts]
+            doc_uuid: str = str(uuid.uuid4())
+            all_metadata: list[dict] = [update_metadata(file_name, doc_uuid, d.metadata) for d in texts]
+            ids: list[str] = get_uuids_for_document_texts(texts)
             self.collection.add(documents=contents, metadatas=all_metadata, ids=ids)
             # split and load into vector db
             print(f"Found {len(data)} parts in file {file_path}")
         except Exception as e:
             print(f"process_file: Error parsing file {file_path}.  {e}")
 
-    def load_file_bytes(self, file_bytes, file_name):
+    def load_file_bytes(self, file_bytes: bytes, file_name: str) -> None:
         _, file_extension = os.path.splitext(file_name)
         with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension, prefix=file_name) as fp:
             fp.write(file_bytes)
