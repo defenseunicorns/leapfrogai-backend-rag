e9ab7ecde3b91afd5ca34e928043d1ac808c8fb4
gharvey <gato.harvey@defenseunicorns.com>
2024-02-15 14:02:13 -0800

    Add more debug output

--- tests/test_main.py
+++ tests/test_main.py
@@ -97,7 +97,6 @@ def test_upload(collection):
 
             test_collection = main.doc_store.collection
             while test_collection.count() == 0:
-                print(test_collection.count())
                 sleep(1)
 
             response = client.get("/list/")
@@ -113,7 +112,9 @@ def test_query_raw(collection):
 
             test_collection = main.doc_store.collection
             while test_collection.count() == 0:
+                print("inside" + test_collection.count())
                 sleep(1)
+            print("outside" + test_collection.count())
 
             response = client.post("/query/raw", json={"input": "some input value",
                                                        "collection_name": TEST_COLLECTION_NAME})
